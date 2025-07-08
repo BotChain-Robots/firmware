a6a2f0a51d9cbe8ad6adbf764e4ba950dfab1808
Johnathon Slightham <slighthamj@gmail.com>
2025-07-08 03:34:27 -0400

    Add PSRAM

--- main/main.cpp
+++ main/main.cpp
@@ -5,6 +5,7 @@
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 
 #include "WifiManager.h"
 #include "mDNSDiscoveryService.h"
@@ -12,8 +13,12 @@
 #include "ConfigManager.h"
 #include "constants/tcp.h"
 #include "LoopManager.h"
+#include "esp_log.h"
 
 extern "C" [[noreturn]] void app_main(void) {
+    ESP_LOGI("MEM", "Free internal RAM: %d", heap_caps_get_free_size(MALLOC_CAP_8BIT));
+    ESP_LOGI("MEM", "Free PSRAM: %d", heap_caps_get_free_size(MALLOC_CAP_SPIRAM));
+
     ConfigManager::init_config();
 
     const auto manager = std::make_unique<WifiManager>();
